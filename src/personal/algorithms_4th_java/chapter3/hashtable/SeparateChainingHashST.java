f92d3a1e247a9f2cd93ff50cf4eadfac7c2ed846
Terence-cn <1035260902@qq.com>
2020-07-09 17:56:11 +0800

    查找-散列表

---
+++ src/personal/algorithms_4th_java/chapter3/hashtable/SeparateChainingHashST.java
@@ -0,0 +1,49 @@
+package personal.algorithms_4th_java.chapter3.hashtable;
+
+import edu.princeton.cs.algs4.Queue;
+import edu.princeton.cs.algs4.SequentialSearchST;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 基于拉链法的散列表
+ * @param <Key>
+ * @param <Value>
+ */
+public class SeparateChainingHashST<Key,Value> {
+    private int N;//键值对总数
+    private int M;//散列表的大小
+    private SequentialSearchST<Key,Value>[] st;//存放链表对象的数组。SequentialSearchST是一个无序符号表
+
+    public SeparateChainingHashST(){
+        this(997);
+    }
+    public SeparateChainingHashST(int M){
+        this.M=M;
+        st=(SequentialSearchST<Key,Value>[])new SequentialSearchST[M];
+        for(int i=0;i<M;i++)
+            st[i]=new SequentialSearchST<>();
+    }
+
+    private int hash(Key key){
+        return (key.hashCode()&0x7fff_fff_f)%M;
+    }
+    private Value get(Key key){
+        return st[hash(key)].get(key);
+    }
+    private void put(Key key,Value value){
+        st[hash(key)].put(key,value);
+    }
+
+    public Iterable<Key> keys(){
+        Queue<Key> keys = new Queue<Key>();
+        for (SequentialSearchST<Key, Value> keyValueSequentialSearchST : st) {
+            Iterable<Key> queue = keyValueSequentialSearchST.keys();
+            for (Key key : queue) {
+                keys.enqueue(key);
+            }
+        }
+        return keys;
+    }
+}
