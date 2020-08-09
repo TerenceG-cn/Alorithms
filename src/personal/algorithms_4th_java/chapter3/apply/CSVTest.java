1c2e2e4d4332cbde48915c928bf5558854b4526a
Terence-cn <1035260902@qq.com>
2020-08-09 21:22:23 +0800

    3.5 应用

---
+++ src/personal/algorithms_4th_java/chapter3/apply/CSVTest.java
@@ -0,0 +1,10 @@
+package personal.algorithms_4th_java.chapter3.apply;
+
+import edu.princeton.cs.algs4.In;
+
+public class CSVTest {
+    public static void main(String[] args){
+        In in = new In("https://introcs.cs.princeton.edu/java/data/amino.csv");
+
+    }
+}
---
+++ src/personal/algorithms_4th_java/chapter3/apply/DeDup.java
@@ -0,0 +1,23 @@
+package personal.algorithms_4th_java.chapter3.apply;
+
+import edu.princeton.cs.algs4.SET;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * DeDup过滤器，调用SET或者HashSET来去掉输入流中的重复项的程序
+ */
+public class DeDup {
+    private DeDup() {}
+
+    public static void main(String[] args) {
+        SET<String> set = new SET<String>();
+        while (!StdIn.isEmpty()) {
+            String key = StdIn.readString();
+            if (!set.contains(key)) {
+                set.add(key);
+                StdOut.println(key);
+            }
+        }
+    }
+}
